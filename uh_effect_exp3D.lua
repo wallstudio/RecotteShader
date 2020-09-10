9e5b7fb59995af75f0d483611a2cade1a09f699d
UpHash <yukawallstudio@gmail.com>
2020-09-10 23:38:37 +0900

    パラメータ拡張に反転機能追加

--- uh_effect_exp3D.lua
+++ uh_effect_exp3D.lua
@@ -6,7 +6,7 @@ function GetInfo()
         name = "uh_effect_exp3D",
         displayname = {
             en = "uh_effect_exp3D",
-            ja = "UH_3Dパラメータ拡張"
+            ja = "UH_パラメータ拡張"
         },
         tag = "effect",
         -- AF_Scale | AF_Angle | AF_Color | AF_Bounds | AF_Scale| AF_Shader | AF_UV
@@ -26,6 +26,8 @@ function InitEffect()
     AddProperty(NewProperty("exp3D_contrast", { ja="コントラスト", en="exp3D_contrast"}, "int", nil, 0));
     AddProperty(NewProperty("exp3D_gamma", { ja="ガンマ", en="exp3D_gamma"}, "float", nil, 1.14));
     AddProperty(NewProperty("exp3D_color", {ja="乗算カラー", en="exp3D_color"}, "color", nil, RGBA256(255, 255, 255, 255)));
+    AddProperty(NewProperty("exp3D_flipX", { ja="左右反転", en="exp3D_flipX"}, "bool", nil, false));
+    AddProperty(NewProperty("exp3D_flipY", { ja="上下反転", en="exp3D_flipY"}, "bool", nil, false));
 end
 
 function ApplyEffect(effInfo, param)
@@ -42,5 +44,21 @@ function ApplyEffect(effInfo, param)
     param.gamma = GetProperty("exp3D_color");
     local c = GetProperty("exp3D_color");
     param.cm = param.cm * MAT44.Scale(c.r, c.g, c.b);
+
+    if GetProperty("exp3D_flipX") then
+        param.uv.x = 1;
+        param.uv.w = -1;
+    else
+        param.uv.x = 0;
+        param.uv.w = 1;
+    end
+    if GetProperty("exp3D_flipY") then
+        param.uv.y = 1;
+        param.uv.h = -1;
+    else
+        param.uv.y = 0;
+        param.uv.h = 1;
+    end
+
     return param
 end
